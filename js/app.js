148c7b42974a94b29466d4965daeca4215cf57b5
Yamir Serret <88166789+26Lunas@users.noreply.github.com>
2022-12-15 19:31:37 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,40 @@
+$(function() {
+    console.log("Jquery esta funcionando");
+    
+    $('.btn-miWork').hover(function () {
+
+        let iconoFlecha = document.getElementById('iconoFlecha');
+        iconoFlecha.classList.toggle('rotar');
+
+        let contTitle = document.getElementById('cont-title');
+        contTitle.classList.toggle('btn-miWork-hover');
+
+    })
+   
+})
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 900, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+    
